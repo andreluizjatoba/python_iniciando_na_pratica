393a18f65d7f63c902d834da10fa9445caf34d8b
andre.jatoba <andre.jatoba@gmail.com>
2024-07-15 21:19:39 -0300

    Função input

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+# Python 2 ASCII
+# coding=utf-8
+#
+# https://en.wikipedia.org/wiki/ASCII
+#
+# print(chr(65))
+#
+#
+# Python 3 unicode
+#
+# https://peps.python.org/pep-0263/
+#
+
 print("****************************************")
 print("*** School of Net - Caixa Eletrônico ***")
 print("****************************************")
+
+account_typed = input('Digite sua conta: ')  # String
+password_typed = input('Digite sua senha: ')  # String
+
+print(account_typed)
+print(password_typed)
+
+print()
